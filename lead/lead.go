e42b62f21282880df014258c79fd227aca52f7b1
Hari Kartha <karthahari2002@gmail.com>
2023-09-25 02:56:50 +0530

    Created funcs

--- lead/lead.go
+++ lead/lead.go
@@ -1,6 +1,12 @@
 package lead
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/gofiber/fiber"
+	"github.com/harikartha02/go-fiber-crm/database"
+	"github.com/harikartha02/go-fiber-crm/lead"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
 
 type Lead struct {
 	gorm.Model
@@ -8,4 +14,45 @@ type Lead struct {
 	Company string
 	Email   string
 	Phone   int
+}
+
+func GetLeads(c *fiber.Ctx) {
+	db := database.DBConn
+	var leads []Lead
+	db.Find(&leads)
+	c.JSON(leads)
+
+}
+
+func GetLead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx) {
+	db := database.DBConn
+	lead := new(Lead)
+	if err := c.BodyParser(lead); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	db.Create(&lead)
+	c.JSON(lead)
+
+}
+
+func DeleteLead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead
+	db.First(&lead, id)
+	if lead.Name == "" {
+		c.Status(500).Send("No lead found with ID")
+		return
+	}
+	db.Delete(&lead)
+	c.Send("Lead successfully deleted")
 }
